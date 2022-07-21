8bc2c189d1382c47fc3d5f05a2845c9d58987924
Imge Yüzüncüoglu <86414587+Arellias@users.noreply.github.com>
2022-07-21 13:19:47 +0200

    Update baseline_first.py
    Finished the first baseline

--- baseline_first.py
+++ baseline_first.py
@@ -1,43 +1,18 @@
 """
-Das folgende Programm präsentiert die erste Baseline.
-Baselines werden verwendet, um vergleichen zu können, ob sich das 
-implementierte Programm "lohnt" oder keinen Unterschied zu vorherigen
-Methoden macht.
+    The first baseline will be the grouptag for intensifier,
+    focus- and gradparticel from the someweta module itself. 
 
-Die 1. Baseline: 
-    POS-Tags ADJD & ADJA herausfiltern und das links davon
-    stehende Element als Intensivierer markieren, solange es kein 
-    Verb ist
-    Also sprich: einfach eine heuristische Methode
-
-Die 2. Baseline:
-    Kann ein Modell von dem Empirist Modell darstellen mit ihrer 
-    Sammelklasse von Fokus- Graduierungs- und Intensitätspartikeln. 
-    Das von someweta
-
-Könnte auch ne 3. Baseline einbauen, bei der mit den Wörterlisten
-nach weiteren Intensivierern geschaut wird, aber weiß nicht, inwieweit
-das schon zu viel wäre .. hmm
+    Therefore the baseline is nothing but the def pos_tagging(sentences)
+    where the tokens tagged with ... will be marked as intensifiers.
 """
 
-# if-Bedingung, ob es sich bei Token um ein Adjektiv handelt
-    # if token == ADJD or token == ADJA:
-        # gucken, was das Objekt links daneben ist:
-            # if token[-1] nicht Verb, nicht Determinant: markiere
-                # es als Intensivierer
-            #else: pass
-    # else: pass
+def baseline_first(tagged_tokens):
+    y_pred = list()
+    for token_tag in enumerate(tagged_tokens):
+        #print(token_tag[1][1])
+        if token_tag[1][1] == "PTKIFG":
+            y_pred.append(1)
+        else:
+            y_pred.append(0)
 
-
-def itsf_identification(sentences):
-    pass
-
-def run_script(filenames):
-    pass
-
-
-if __name__ == "__main__":
-
-    filename = "test_on_data5.tsv"
-
-    #run_script(filenames)
+    return y_pred
