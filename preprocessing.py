8f24dcebaae9798cbc7664f34b36d60f960bb633
Imge Yüzüncüoglu <86414587+Arellias@users.noreply.github.com>
2022-07-23 07:59:30 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,28 +1,12 @@
 """
-    This file is for the preprocessing of the data.
-    Good thing is that the data is already tokenized.
-    That will be helpful.
-    Results are supposed to be:D.h.:
-    > PoS-Tagged Tokens
-    > Splitting adjectivecompounds that were annotated as intensifiers
-
-    So to be able to use this preprocessing file unproblematicly,
-    the data has to be already tokenized.
-"""
-
-### To Do ###
-# split adjective compounds that are marked as intensifiers
-# add comments 
-# extract if intensifier or no in seperate list ?
-
-# module for PoS-Tagging
-from someweta import ASPTagger
-
-# import first baseline 
-from baseline_first import baseline_first
-
-### PREPROCESSING  ###
-
+    Thit file contains the steps for preprocessing
+    the data. 
+    The functions are:
+    readfile()              : Reading in the file
+    get_informations()      : Extracts the featurevec, gold_lables and sentences
+    test_and_train_data()   : Divides data into train and test data
+    pos_tagging()           : PoS-tags the data
+    """
 def readfile(filename):
     """
         Reads the file and saves its content within a variable.
@@ -33,67 +17,110 @@ def readfile(filename):
 
     return file_input
 
-def get_information(file_input):
+def get_informations(file_input):
     """
-        This function will generate sentences out of the tokens.
-        Because the data is already tokenized, the dot can 
-        simply be used as a sentence-end marker.
+        Maingoal of this function is to extract the specific
+        information of each inputline. 
+        Extracted will be: 
+            inputvectors: Inputvectors (Feature1, Feature2, Feature3)
+                Feature 1   : Is token in front of an adjective? (ifoa)
+                Feature 2   : Is the adjective predicative or attributive?
+                Feature 3   : Is the adcetive semantically graduable?  # still needs to be added in annotation
+            
+            gold_labels : The annotation if token is intensifier or not
+            sentences   : Sentences markend by the token "." (possible, because
+                            the data is already tokenized)
+
     """
 
-    # list in which one sentence will be preserved
-    sentence = []
     # list which will contain all the sentences in form of
     # sentences = [["Ein", "Satz", "ist", "eine", "Liste", "von", "Tokens", "."],
     #             ["Zeitfliegen", "mögen", "einen", "Pfeil", "."]]
     sentences = []
+    
+    # list in which one sentence will be preserved
+    sentence = []
 
-    token_information = list()
+    # features
+    # feature1 = list()
+    # feature2 = list()
+    # feature3 = list()
 
-    y_gold = list ()
+    featurevec = list()
+
+    # gold lables
+    gold_lables = list()
 
     # go through the input from the file
-    for i in range(len(file_input)):
-
+    for index, item in enumerate(file_input):
         # ...split the input...
-        content = file_input[i].split("\t")
-        token_information.append(content)
-        y_gold.append(content[-2])
-        # extract the token
-        token = content[0]
-        # append the token to generate the one sentence
-        sentence.append(token)
+        content = item.split("\t")
 
-        if token == ".":
+        # extract feature1
+        # feature1.append(content[1])
+        # # extract feature2
+        # feature2.append(content[2])
+        # extract feature3 # still needs to be added
+        # feature3 = content[3]
+
+        feature1 = content[1]
+
+        # für Naive Bayes gaussisch müssen alles Zahlen sein, therefore
+        if content[2] == "pred":
+            feature2 = 1
+        elif content[2] == "att":
+            feature2 = 2
+        elif content[2] == "None":
+            feature2 = 0
+        #feature2 = content[2]
+
+        featurevec.append((feature1, feature2))
+
+        gold_lables.append(content[3]) # later on element 4, if feature3 is added
+        # gold_lables.append(content[4])
+
+        # generate a sentence for the pos-tagging
+        sentence.append(content[0])
+
+        if content[0] == ".":
             sentences.append(sentence)
-            sentence = []
+            sentence = list()
 
-    return token_information, sentences, y_gold
+    # inputvectors
+    #inputvectors = zip(feature1, feature2) # add feature3 later on
+    #inputvectors = zip(feature1, feature2, feature3)
 
+    return featurevec, gold_lables, sentences
 
-def features_and_labels(token_information):
-    """ 
-        Unterteilung von Train und Testdata 
-        Sowie die jeweilige Selektierung der FEatures und Labels
+def test_and_train_data(featurevec, gold_lables):
+    """"
+        This function divides the data in 
+        train and test data.
     """
+    data_amount = len(featurevec)
+    train_amount = data_amount / 100 * 80
+
     train_features = list()
-    train_gold = list()
+    train_lables = list()
+
     test_features = list()
-    test_gold = list()
-    number_data = len(token_information)
-    train_data_len = number_data/100*80
-    for index, data in enumerate(token_information):
-        if index <= train_data_len:
-            train_features.append(token_information[1],token_information[2])
-            train_gold.append(token_information[3])
+    test_lables = list()
+
+    for index, (input, gold) in enumerate(zip(featurevec, gold_lables)):
+
+        if index <= train_amount:
+            train_features.append(input)
+            train_lables.append(gold)
+
         else:
-            test_features.append(token_information[1],token_information[2])
-            test_gold.append(token_information[3])
+            test_features.append(input)
+            test_lables.append(gold)
 
-    return train_features, train_gold, test_features, test_gold
 
+    return train_features, train_lables, test_features, test_lables
 
 def pos_tagging(sentences):
-    
+    from someweta import ASPTagger
     """
         This function uses the pretrained model someweta from empirist 
         to tag the tokens which are within the sentences."""
@@ -114,53 +141,3 @@ def pos_tagging(sentences):
             tagged_tokens.append(tagged_sentence[index])
     
     return tagged_tokens
-
-
-### SECOND BASELINE ###
-def baseline_second(tagged_tokens):
-    """
-    hier ist noch ein Denkfehler, der ausgeglichen werden muss
-    token muss ja nciht v sein, das davor muss n v sein, oder ein .
-    sie ist voll cool.
-    gar nicht 
-    like this 
-    
-    """
-    intensifier_pred = list()
-    for index, token in enumerate(tagged_tokens):
-        try:
-            # if token in front of adjective:
-            if tagged_tokens[index+1][1] == "ADJD" or tagged_tokens[index+1][1] == "ADJA":
-                #check, if is a verb
-                if tagged_tokens[index][1][0] == "V":
-                    intensifier_pred.append(0)
-                else:
-                    intensifier_pred.append(1)
-            else:
-                intensifier_pred.append(0)
-        except:
-            intensifier_pred.append(0)
-
-    return intensifier_pred
-
-### TAFIN   ###
-
-### RUN SCRIPT ###
-def run_script(filename):
-    file_input = readfile(filename)
-    token_information, sentences, y_gold = get_information(file_input)
-    #print(len(y_gold))
-    tagged_tokens = pos_tagging(sentences)
-    #print(tagged_tokens)
-    intens_pred_first = baseline_first(tagged_tokens)
-    #print(intens_pred_first)
-    for pred, gold in zip(intens_pred_first, y_gold):
-        if int(pred) != int(gold):
-            print(pred, "\t", gold)
-    print("FEDDIG")
-
-if __name__ == "__main__":
-
-    filename = "test_on_data5.txt"
-
-    run_script(filename)
