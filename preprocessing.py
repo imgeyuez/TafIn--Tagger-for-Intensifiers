9cd3b1bffd29e8c0697f66e6bae0d66b96e71f1b
Imge Yüzüncüoglu <86414587+Arellias@users.noreply.github.com>
2022-07-25 22:03:48 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,13 @@
 """
-    This file contains the steps for preprocessing
+    Thit file contains the steps for preprocessing
     the data. 
     The functions are:
     readfile()              : Reading in the file
     get_informations()      : Extracts the featurevec, gold_lables and sentences
     test_and_train_data()   : Divides data into train and test data
     pos_tagging()           : PoS-tags the data
-    """
+"""
+
 def readfile(filename):
     """
         Reads the file and saves its content within a variable.
@@ -15,9 +16,14 @@ def readfile(filename):
     with open(filename, encoding="UTF-8-sig") as file:
         file_input = file.readlines()
 
-    return file_input
+    data_input = list()
 
-def generatesentences(file_input):
+    for input in file_input:
+        data_input.append(input.split("\t"))
+
+    return data_input
+
+def generatesentences(data_input):
     # is needed for the pos-tagging
      # list which will contain all the sentences in form of
     # sentences = [["Ein", "Satz", "ist", "eine", "Liste", "von", "Tokens", "."],
@@ -27,21 +33,79 @@ def generatesentences(file_input):
     # list in which one sentence will be preserved
     sentence = []
 
-    data_len = len(file_input)
-    for index, token in enumerate(file_input):
+    data_len = len(data_input)
+    for index, data in enumerate(data_input):
         #if index == data_len-1:
-        token_content = token.split("\t")
 
-        if token_content[0] != ".":
-            sentence.append(token_content[0])
+        if data[0] != ".":
+            sentence.append(data[0])
         else:
-            sentence.append(token_content[0])
+            sentence.append(data[0])
             sentences.append(sentence)
             sentence = list()
 
     return sentences
 
-def pos_tagging(file_input, sentences):
+def train_test_data(data_input, sentences):
+    
+    train_data = list()
+    test_data = list()
+
+    train_sentences = list()
+    test_sentences = list()
+    
+    data_amount = len(sentences)
+    train_amount = data_amount / 100 * 80 #5,6
+
+    token_number = 0
+
+    for index, sentence in enumerate(sentences):
+        if index <= train_amount:
+            train_sentences.append(sentence)
+            for token in sentence:
+                train_data.append(data_input[token_number])
+                token_number += 1
+
+        else:
+            test_sentences.append(sentence)
+            for token in sentence:
+                test_data.append(data_input[token_number])
+                token_number += 1
+
+    return train_data, test_data, train_sentences, test_sentences
+
+# def pos_tagging(train_sentences, test_sentences):
+#     from someweta import ASPTagger
+#     """
+#         This function uses the pretrained model someweta from empirist 
+#         to tag the tokens which are within the sentences.
+#     """
+
+#     # loads pretrained model form someweta
+#     model = "german_web_social_media_2020-05-28.model"
+
+#     asptagger = ASPTagger()
+#     asptagger.load(model)
+
+#     # list in which the tokens with their tags will be saved in
+#     # form: tuple(token, tag)
+#     train_tokens_tags = list()
+
+#     for sentence in train_sentences:
+#         tagged_sentence = asptagger.tag_sentence(sentence)
+#         for index in range(len(tagged_sentence)):
+#             train_tokens_tags.append(tagged_sentence[index])
+
+#     test_tokens_tags = list()
+
+#     for sentence in test_sentences:
+#         tagged_sentence = asptagger.tag_sentence(sentence)
+#         for index in range(len(tagged_sentence)):
+#             test_tokens_tags.append(tagged_sentence[index])
+
+#     return train_tokens_tags, test_tokens_tags
+
+def pos_tagging(sentences):
     from someweta import ASPTagger
     """
         This function uses the pretrained model someweta from empirist 
@@ -62,23 +126,15 @@ def pos_tagging(file_input, sentences):
         tagged_sentence = asptagger.tag_sentence(sentence)
         for index in range(len(tagged_sentence)):
             tokens_tags.append(tagged_sentence[index])
-    
-    # adding PoS-Information to the data in a new datafile
-    new_data = list()
 
-    for index, token in enumerate(file_input):
-        content = token.split("\t")
-        pos_tag = tokens_tags[index][1]
-        content.insert(1, pos_tag)
-        new_data.append(content)
+    return tokens_tags
     
-    return new_data
-
-def splitcompounds(new_data):
+def splitcompounds(data_variable):
 
     compounds = list()
 
-    for index, token in enumerate(new_data):
+    for index, token in enumerate(data_variable):
+        # if intensifier
         if token[-2] == "1":
             if token[1] == "ADJD" or token[1] == "ADJA":
                 compounds.append([index, token])
@@ -91,22 +147,25 @@ def splitcompounds(new_data):
         [(0.1740327189765392, 'Zucker', 'Süß'), (-1.5975678869950305, 'Zuck', 'Ersüß'), (-2.1849865951742626, 'Zuc', 'Kersüß'), 
         (-3, 'Zucke', 'Rsüß')]  
     """
-
-    # doch lieber in dictionary umwandeln?? 
-    splittet_compounds = list()
+    #splitted_compounds = dict()
+    splitted_compounds = list()
 
     for compound in compounds:
         # form of compound:
         # compund = [67, ['klitzeklein', 'ADJD', '0', 'None', '1', '\n']] 
-
+        
+        # form of splitted_compound = 
+        # [[63, 'schweineschwer', ['Schweine', 'Schwer']], [67, 'klitzeklein', ['Klitze', 'Klein']]]
         splitted_compound = splitter.split_compound(compound[1][0])
         lexem1 = splitted_compound[0][1]
         lexem2 = splitted_compound[0][2]
-        splittet_compounds.append([compound[1][0], [lexem1, lexem2]])
+        #splitted_compounds[compound[0]] = compound[1][0], [lexem1, lexem2]
+        splitted_compounds.append([compound[0], compound[1][0], [lexem1, lexem2]])
 
-    return compounds, splittet_compounds
 
-def insert_compounds(new_data, splitted_compounds):
+    return compounds, splitted_compounds
+
+def insert_compounds(data_variable, splitted_compounds):
     
     # hier token noch einmal pos-taggen und annotationen
     # ergänzen und dann
@@ -124,14 +183,11 @@ def insert_compounds(new_data, splitted_compounds):
 
     # create their pos-tag and annotation
     index_token_tag = dict()
-    #compound_index = list()
 
     list_of_fail_annotated = list()
 
     for compound in splitted_compounds:
-        annot = list()
-        # #print(compound[0])
-        # compound_index.append(compound[0])
+        
         tokens_tags = list()
         tagged_lexems = asptagger.tag_sentence(compound[2])
         tokens_tags.append(tagged_lexems)
@@ -162,125 +218,65 @@ def insert_compounds(new_data, splitted_compounds):
     # {63: (['Schweine', 'NN', '1', 'pred', '1'], ['Schwer', 'ADJD', '0', 'None', '0']), 
     # 67: (['Klitze', 'NN', '1', 'pred', '1'], ['Klein', 'ADJD', '0', 'None', '0'])}
 
-
-    new_new_data = list()
+    new_data = list()
     keys = index_token_tag.keys() 
 
-    for index, token in enumerate(new_data):
+    for index, token in enumerate(data_variable):
         # wenn der index ein Kompositum ist,
         if index in keys:
         # werden anstelle des Kompositums
         # die beiden Lexeme eingetragen
             tokens = index_token_tag.get(index)
-            new_new_data.append(tokens[0])
-            new_new_data.append(tokens[1])
+            new_data.append(tokens[0])
+            new_data.append(tokens[1])
 
         # ist das Token bei dem Index kein 
         # Kompositum, können die Daten einfach
         # übernommen werden 
         else:
-            new_new_data.append(token)
+            new_data.append(token)
 
-    return new_new_data
+    return new_data
 
-def get_informations(file_input):
-    """
-        Maingoal of this function is to extract the specific
-        information of each inputline. 
-        Extracted will be: 
-            inputvectors: Inputvectors (Feature1, Feature2, Feature3)
-                Feature 1   : Is token in front of an adjective? (ifoa)
-                Feature 2   : Is the adjective predicative or attributive?
-                Feature 3   : Is the adcetive semantically graduable?  # still needs to be added in annotation
-            
-            gold_labels : The annotation if token is intensifier or not
-            sentences   : Sentences markend by the token "." (possible, because
-                            the data is already tokenized)
 
-    """
+### RUN SCRIPT ###
+def run_script(filename):
 
-    # list which will contain all the sentences in form of
-    # sentences = [["Ein", "Satz", "ist", "eine", "Liste", "von", "Tokens", "."],
-    #             ["Zeitfliegen", "mögen", "einen", "Pfeil", "."]]
-    sentences = []
+    data_input = readfile(filename)
+
+    sentences = generatesentences(data_input)
+
+    #print(train_test_data(data_input, sentences))
+    train_data, test_data, train_sentences, test_sentences = train_test_data(data_input, sentences)
+    #print(train_data)
+    #print(test_data)
+    #print(test_sentences)
+    #print(train_sentences)
     
-    # list in which one sentence will be preserved
-    sentence = []
+    #train_tokens_tags, test_tokens_tags = pos_tagging(train_sentences, test_sentences)
+    #print(test_tokens_tags)
+    train_tokens_tags = pos_tagging(train_sentences)
+    test_tokens_tags = pos_tagging(test_sentences)
 
-    # features
-    # feature1 = list()
-    # feature2 = list()
-    # feature3 = list()
+    # update the train_data
+    for index, token in enumerate(train_data):
+        pos_tag = train_tokens_tags[index][1]
+        token.insert(1, pos_tag)
 
-    featurevec = list()
+    # update the test_data
+    for index, token in enumerate(test_data):
+        pos_tag = test_tokens_tags[index][1]
+        token.insert(1, pos_tag)
 
-    # gold lables
-    gold_lables = list()
-
-    # go through the input from the file
-    for index, item in enumerate(file_input):
-        # ...split the input...
-        content = item.split("\t")
-
-        # extract feature1
-        # feature1.append(content[1])
-        # # extract feature2
-        # feature2.append(content[2])
-        # extract feature3 # still needs to be added
-        # feature3 = content[3]
-
-        feature1 = content[1]
-
-        # für Naive Bayes gaussisch müssen alles Zahlen sein, therefore
-        if content[2] == "pred":
-            feature2 = 1
-        elif content[2] == "att":
-            feature2 = 2
-        elif content[2] == "None":
-            feature2 = 0
-        #feature2 = content[2]
-
-        featurevec.append((feature1, feature2))
-
-        gold_lables.append(content[3]) # later on element 4, if feature3 is added
-        # gold_lables.append(content[4])
-
-        # generate a sentence for the pos-tagging
-        sentence.append(content[0])
-
-        if content[0] == ".":
-            sentences.append(sentence)
-            sentence = list()
-
-    # inputvectors
-    #inputvectors = zip(feature1, feature2) # add feature3 later on
-    #inputvectors = zip(feature1, feature2, feature3)
-
-    return featurevec, gold_lables, sentences
-
-def test_and_train_data(featurevec, gold_lables):
-    """"
-        This function divides the data in 
-        train and test data.
-    """
-    data_amount = len(featurevec)
-    train_amount = data_amount / 100 * 80
-
-    train_features = list()
-    train_lables = list()
-
-    test_features = list()
-    test_lables = list()
-
-    for index, (input, gold) in enumerate(zip(featurevec, gold_lables)):
-
-        if index <= train_amount:
-            train_features.append(input)
-            train_lables.append(gold)
-
-        else:
-            test_features.append(input)
-            test_lables.append(gold)
+    compounds, train_splitted_compounds = splitcompounds(train_data)
+    compounds, test_splitted_compounds = splitcompounds(test_data)
 
 
-    return train_features, train_lables, test_features, test_lables
+    new_train_data = insert_compounds(train_data, train_splitted_compounds)
+    print(new_train_data)
+
+if __name__ == "__main__":
+
+    filename = "preproc_test.txt"
+
+    run_script(filename)
