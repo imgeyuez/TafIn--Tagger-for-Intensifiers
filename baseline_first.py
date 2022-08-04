1fb6b45142370d480a3dbd33febb3b038988de18
Imge Yüzüncüoglu <86414587+Arellias@users.noreply.github.com>
2022-08-04 21:06:23 +0200

    Update baseline_first.py

--- baseline_first.py
+++ baseline_first.py
@@ -2,20 +2,30 @@
     The first baseline will be the grouptag for intensifier,
     focus- and gradparticel from the someweta module itself. 
 
-    Therefore the baseline is nothing but the def pos_tagging(sentences)
-    where the tokens tagged with PTKIFG will be marked as intensifiers.
+    Therefore the baseline is nothing but the results of
+    def pos_tagging(sentences) where the tokens tagged 
+    with PTKIFG will be marked as intensifiers.
 """
 
-def baseline_first(test_data):
-    # list for lable predictions
+def baseline_first(x_test):
     y_pred = list()
-    # go through each token in the data
-    for token in test_data:
-        # if it has the tag, append 1 for the lable "intensifier"
-        if token[1] == "PTKIFG":
-            y_pred.append(1)
-        # else append 0 for "not intensifier"
-        else:
-            y_pred.append(0)
+
+    for doc in x_test:
+        sent_pred = list()
+
+        for index, token in enumerate(doc):
+            # if token is marked as an intensifier
+            if token[1] == "PTKIFG":
+                # check, if token before is also one
+                # if not, it is the beginning of an intensifier
+                # if yes, it is the continuation of an intensifier
+                if doc[index-1] !="PTKIFG":
+                    sent_pred.append("B-ITSF")
+                else:
+                    sent_pred.append("I-ITSF")
+            else:
+                sent_pred.append("O")
+
+        y_pred.append(sent_pred)
 
     return y_pred
