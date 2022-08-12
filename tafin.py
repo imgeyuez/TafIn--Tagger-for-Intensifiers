cb10186e7b8a69e1201a63ba51eca6062bfd31d6
Imge Yüzüncüoglu <86414587+Arellias@users.noreply.github.com>
2022-08-13 01:09:00 +0200

    Update tafin.py

--- tafin.py
+++ tafin.py
@@ -1,44 +1,11 @@
-from _clean import readfile
-from _clean import get_sentences
-from _clean import pos_tagging
-from _clean import split_compounds
-from _clean import tag_compounds
-from _clean import labeling
-from _clean import annotation
-from _clean_2 import test_and_train
+from _preproc import train_test
 
-from sklearn.model_selection import train_test_split
 import pycrfsuite
+import numpy as np
+from sklearn.metrics import classification_report
 
-#filename = "0_test_data.txt"
-filename = "8997_blog.xml.tsv"
+training_data, test_data = train_test()
 
-file_data = readfile(filename)
-sentences = get_sentences(file_data)
-tokens_tags = pos_tagging(sentences)
-splitted_compounds = split_compounds(sentences, tokens_tags)
-tagged_compounds = tag_compounds(sentences, splitted_compounds)
-keys = tagged_compounds.keys() 
-labels = labeling(sentences, tokens_tags)
-work_annot = annotation(tokens_tags, labels)
-documents = list()
-for index, annot in enumerate(work_annot):
-        document = list()
-        for i, token in enumerate(annot):
-            if token[0] in keys:
-                #print(token)
-                tokens = tagged_compounds.get(token[0])
-                document.append(tokens[0])
-                document.append(tokens[1])
-
-            else:
-                document.append(token)
-        
-        documents.append(document )
-
-#train_data, test_data = test_and_train(documents)
-
-#print(train_data)  
 """
 So schaut die train data aus:
     [('Ich', 'PPER', 'O'), ('hab', 'VAFIN', 'O'), ('dann', 'ADV', 'O'), 
@@ -54,6 +21,7 @@ So schaut die train data aus:
     ('richtigen', 'ADJA', 'O'), ('Restaurant', 'NN', 'O'), (',', '$,', 'O'), 
     ('Freitagsabends', 'NN', 'O'), ('.', '$.', 'O')]]
 """
+
 ########## here starts the model ##########
 
 def wordfeatures(doc, i):
@@ -73,17 +41,9 @@ def wordfeatures(doc, i):
     # features I want from all words 
     features = {
         "bias": 1.0,
-        "word": token,
-        "word.islower()": token.islower(), 
-        #'word.lower()': token.lower(),
-        #'word[-3:]': token[-3:],
-        #'word[-2:]': token[-2:],
-        #'word.isupper()': token.isupper(),
-        #'word.istitle()': token.istitle(),
-        #"in_list":token in...?,
-        #"word.isdigit()": token.isdigit(),
+        "token": token,
+        "token.islower()": token.islower(), 
         "postag": postag,
-        #'postag[:2]': pos_tag[:2],
     }
 
     # information I want starting from the second token
@@ -92,12 +52,15 @@ def wordfeatures(doc, i):
         prev_postag = doc[i-1][1]
         features.update({
             "prev_token": prev_token,
-            #"in_list":token in...?,
-            #"prev_isdigit()": prev_token.isdigit(),
             "prev_postag": prev_postag,
-            #"prev_label":???
+            "prev_token.islower()": prev_token.islower(), 
+            "prev_label": doc[i-1][2]
         })
 
+    # if it is 0, it marks the beginning of a sentence
+    else:
+        features.update({"bos": True})
+        
     # information I want until the last token 
     if i < len(doc)-1:
         next_token = doc[i+1][0]
@@ -105,47 +68,44 @@ def wordfeatures(doc, i):
         features.update({
             "next_token": next_token,
             "next_token.islower()": next_token.islower(),
-            #"in_list":token in...?,
-            #"next_isdigit()": next_token.isdigit(),
             "next_postag": next_postag,
         })
+
+    # if it is bigger, it marks the end of a sentence
+    else:
+        features.update({"eos": True})
 
     return features
 
 # function for extracting features in documents
 def extract_features(doc):
-    return [wordfeatures(doc, i) for i in range(len(doc))]
+    return (wordfeatures(doc, i) for i in range(len(doc)))
 
 # function fo generating the list of labels for each document
 def get_labels(doc):
     return [label for (token, postag, label) in doc]
     
+x_train = list()
+for doc in training_data:
+    x_train.append(extract_features(doc))
 
-x_inputs = [extract_features(doc) for doc in documents]
-y_outputs = [get_labels(doc) for doc in documents]
+x_test = list()
+for doc in test_data:
+    x_test.append(extract_features(doc))
 
+y_train = [get_labels(doc) for doc in training_data]
 
-x_train, x_test, y_train, y_test = train_test_split(x_inputs, y_outputs, test_size=0.2, shuffle=False)
-#print(x_train)
+y_test = [get_labels(doc) for doc in test_data]
+
 """
     x_train für ein doc schaut aus wie: 
-    [{'bias': 1.0, 'word': 'Es', 'word.islower()': False, 'postag': 'PPER', 'next_token': 'ist', 'next_token.islower()': True, 'next_postag': 'VAFIN'}, {'bias': 1.0, 'word': 'ist', 'word.islower()': True, 'postag': 'VAFIN', 'prev_token': 'Es', 'prev_postag': 'PPER', 'next_token': 'sau', 'next_token.islower()': True, 'next_postag': 'ITJ'}, 
+    [{'bias': 1.0, 'word': 'Es', 'word.islower()': False, 'postag': 'PPER', 'next_token': 'ist', 'next_token.islower()': True, 'next_postag': 'VAFIN'}, 
+    {'bias': 1.0, 'word': 'ist', 'word.islower()': True, 'postag': 'VAFIN', 'prev_token': 'Es', 'prev_postag': 'PPER', 'next_token': 'sau', 'next_token.islower()': True, 'next_postag': 'ITJ'}, 
     {'bias': 1.0, 'word': 'sau', 'word.islower()': True, 'postag': 'ITJ', 'prev_token': 'ist', 'prev_postag': 'VAFIN', 'next_token': 'kalt', 'next_token.islower()': True, 'next_postag': 'ADJD'}, 
     {'bias': 1.0, 'word': 'kalt', 'word.islower()': True, 'postag': 'ADJD', 'prev_token': 'sau', 'prev_postag': 'ITJ', 'next_token': '.', 'next_token.islower()': False, 'next_postag': '$.'}, 
     {'bias': 1.0, 'word': '.', 'word.islower()': False, 'postag': '$.', 'prev_token': 'kalt', 'prev_postag': 'ADJD'}
     ]]
 """
-
-"""     Ich glaube, das brauche ich nicht mehr
-
-# # extrahierung der trainings features und labels
-# x_train = [extract_features(doc) for doc in train_data]
-# y_train = [get_lables(doc) for doc in train_data]
-
-# # extrahierung der test features und labels
-# x_test = [extract_features(doc) for doc in test_data]
-# y_test = [get_lables(doc) for doc in test_data]
-# """
 
 # # training the model
 trainer = pycrfsuite.Trainer(verbose=True)
@@ -158,7 +118,7 @@ for xseq, yseq in zip(x_train, y_train):
 # paramenters of the model
 trainer.set_params({
     # coefficient for L1 penalty
-    'c1': 0.1,
+    'c1': 0.5,
 
     # coefficient for L2 penalty
     'c2': 0.01,  
@@ -171,16 +131,27 @@ trainer.set_params({
     'feature.possible_transitions': True
     })
 
+#print(trainer)
+
 # Provide a file name as a parameter to the train function, such that
 # the model will be saved to the file when training is finished
 trainer.train("crf.model")
 
-
+#print("crf.model")
 # use the trained model
 tagger = pycrfsuite.Tagger()
 tagger.open("crf.model")
 y_pred = [tagger.tag(xseq) for xseq in x_test]
 
-print(y_pred)
-print("\n")
-print(y_test)
+
+# Create a mapping of labels to indices
+labels = {"O": 0, "B-ITSF": 1, "I-ITSF": 2}
+
+# Convert the sequences of tags into a 1-dimensional array
+predictions = np.array([labels[tag] for row in y_pred for tag in row])
+truths = np.array([labels[tag] for row in y_test for tag in row])
+
+# Print out the classification report
+print(classification_report(
+    truths, predictions,
+    target_names=["O", "B-ITSF", "I-ITSF"]))
