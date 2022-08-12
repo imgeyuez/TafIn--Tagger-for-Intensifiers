0af1d63e85be143719b30b4dd2aa5a88c69bb021
Imge Yüzüncüoglu <86414587+Arellias@users.noreply.github.com>
2022-08-13 01:07:53 +0200

    Update baseline_first.py

--- baseline_first.py
+++ baseline_first.py
@@ -2,10 +2,25 @@
     The first baseline will be the grouptag for intensifier,
     focus- and gradparticel from the someweta module itself. 
 
-    Therefore the baseline is nothing but the results of
-    def pos_tagging(sentences) where the tokens tagged 
-    with PTKIFG will be marked as intensifiers.
+    Therefore the baseline is nothing but the def pos_tagging(sentences)
+    where the tokens tagged with ... will be marked as intensifiers.
 """
+from _preproc import train_test
+import numpy as np
+from sklearn.metrics import classification_report
+
+training_data, test_data = train_test()
+
+def get_inputs(doc):
+    return [(token, postag) for (token, postag, label) in doc]
+    
+x_test = [get_inputs(doc) for doc in test_data]
+
+def get_labels(doc):
+    return [label for (token, postag, label) in doc]
+
+y_test = [get_labels(doc) for doc in test_data]
+
 
 def baseline_first(x_test):
     y_pred = list()
@@ -29,3 +44,17 @@ def baseline_first(x_test):
         y_pred.append(sent_pred)
 
     return y_pred
+
+pred_baseline_one = baseline_first(x_test)
+
+# Create a mapping of labels to indices
+labels = {"O": 0, "B-ITSF": 1, "I-ITSF": 2}
+
+# Convert the sequences of tags into a 1-dimensional array
+predictions = np.array([labels[tag] for row in pred_baseline_one for tag in row])
+truths = np.array([labels[tag] for row in y_test for tag in row])
+
+# Print out the classification report
+print(classification_report(
+    truths, predictions,
+    target_names=["O", "B-ITSF", "I-ITSF"]))
